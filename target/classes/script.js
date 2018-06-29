8c4418fb8245874f4db8c126caf03f86708cb963
hide42 <peter5566@yandex.ru>
2018-06-30 03:37:16 +0900

    let's start

---
+++ target/classes/script.js
@@ -0,0 +1,22 @@
+var height = 15;
+var attempt = 4;
+var intS = 0;
+function scrollToEndPage() {
+    if (height < document.body.scrollHeight)
+    {
+        window.scrollTo(0, height);
+        attempt++;
+        height = parseInt(height) + attempt;
+    }
+    else
+    {
+        clearInterval(intS);
+        elementList = document.querySelectorAll(".audio_row__inner")
+        superList= [];
+        elementList.forEach(function(element) {
+            superList.push(element.innerText.replace(/\n/m, ' - ').split(/\n/)[0]);
+        });
+        JSON.stringify(superList)
+    }
+}
+intS = setInterval(scrollToEndPage,10);
